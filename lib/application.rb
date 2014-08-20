c721c890b408c7a6fa39e7297b4d6a541f23b24e
Layne McNish <lmcnish14@gmail.com>
2014-08-20 10:25:41 -0600

    the hash is formatting awkwardly but it is now able to answer the two questions

--- lib/application.rb
+++ lib/application.rb
@@ -1,24 +1,22 @@
 require 'csv'
 require 'awesome_print'
 
-csv = CSV.read("./data/gschool_commute_data.csv", headers: true, header_converters: :symbol, converters: :all).map { |row| Hash[row.map { |c, r| [c, r ]}] }
-ap csv.to_h(:person, )
+commute = CSV.new(File.new('./data/gschool_commute_data.csv'), :headers => true, :header_converters => :symbol, :converters => [:all]).map { |row| row.to_hash }
 
-#
-#
-# text = File.open("./data/gschool_commute_data.csv").read
-#
-# commute = {}
-#
-# csv = CSV.new(text, :headers => true, :header_converters => :symbol)
-# csv.to_a.map { |row| row.to_h }.map { |hash| hash[:person] }.each do |k, v|
-# commute = k[1..-1].zip(v[1..-1])
-#
-# end
-#
-# ap commute
+nate_inbound = commute.select { |person| person[:person] == "Nate" && person[:week] == 4 && person[:day] == "Wednesday" }
+
+p "Nate traveled inbound #{nate_inbound.first[:inbound]} minutes on Wednesday in the fourth week."
+
+def avg(arr)
+  arr.any? ? arr.reduce(:+)/arr.size.to_f : 0.0
+end
+
+average = (commute.each_with_object (Hash.new { |h, k| h[k]=[] }) {
+  |mh, h| mh.keys.each { |k| h[k] << mh[k].to_f  } })
+.each_with_object({}) { |(k, v), h| h[k] = avg(v) }
+
+p "In all 5 weeks, the average inbound commute was #{average[:inbound].to_i} minutes and the average outbound commute was #{average[:outbound].to_i}."
 
 
-# @commute.each do |row|
-#  ap row.first
-# end
+
+
